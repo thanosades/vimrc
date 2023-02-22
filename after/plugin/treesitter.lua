32cf25bd8320de98736b56761c2dd396e7c185c0
Thanos Dimitriades <thanosades@gmail.com>
2023-02-22 14:44:30 +0200

    ignore plugin folder

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -10,12 +10,6 @@ require'nvim-treesitter.configs'.setup {
   auto_install = true,
 
   highlight = {
-    enable = true,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
+    enable = true
   },
 }
